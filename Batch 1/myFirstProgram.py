41cc468c21aa0cb5ce2e27e5f671b3bd6e8fef65
Jude Ogbuibe <jude.ogbuibe@hotmail.com>
2020-08-22 16:15:08 -0500

    First commit of all the codes i have written!

---
+++ Batch 1/myFirstProgram.py
@@ -0,0 +1,9 @@
+
+print("Hello People, Python is Awesome!!!")
+#name = input("What is your name: ")
+#print("Your name is " + name)
+
+firstNumber = int(input("Enter the first Number: "))
+secondNumber = int(input("Enter the second number: "))
+total = firstNumber + secondNumber
+print(f"The sum of the 2 numbers is {total} ")
